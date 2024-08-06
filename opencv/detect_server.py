efc1b1243c1d3dcda62717e376cd2d81e065d023
Sebastian <sebastian_fis@yahoo.de>
2024-08-06 23:22:14 +0200

    Completely rewrote detect_server logic

--- opencv/detect_server.py
+++ opencv/detect_server.py
@@ -24,15 +24,16 @@ python3 opencv/detect_server.py --model all_models/mobilenet_ssd_v2_coco_quant_p
   --labels all_models/coco_labels.txt
 
 """
-import argparse
-import cv2
-import numpy as np
-import os
-import logging
-from imutils.video import VideoStream
 from flask import Response
 from flask import Flask
 from flask import render_template
+import threading
+import datetime
+import time
+import os
+import cv2
+import argparse
+import logging
 
 from pycoral.adapters.common import input_size
 from pycoral.adapters.detect import get_objects
@@ -40,36 +41,63 @@ from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
 
-def run_server(render_gen):
-    logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--bitrate', type=int, default=1000000,
-                        help='Video streaming bitrate (bit/s)')
-    parser.add_argument('--loop', default=False, action='store_true',
-                        help='Loop input video file')
-    parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir, default_model))
-    parser.add_argument('--labels', help='label file path',
-                        default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=3,
-                        help='number of categories with highest score to display')
-    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default=0)
-    parser.add_argument('--threshold', type=float, default=0.5,
-                        help='classifier score threshold')
-    args = parser.parse_args()
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful when multiple browsers/tabs
+# are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+# initialize a flask object
+app = Flask(__name__)
+# initialize the video stream and allow the camera sensor to
+# warmup
+time.sleep(2.0)
 
-    gen = render_gen(args)
-    camera = make_camera(args.source, next(gen), args.loop)
-    assert camera is not None
 
-    with StreamingServer(camera, args.bitrate) as server:
-        def render_overlay(tensor, layout, command):
-            overlay = gen.send((tensor, layout, command))
-            server.send_overlay(overlay if overlay else EMPTY_SVG)
+@app.route("/")
+def index():
+    # return the rendered template
+    return render_template("index.html")
 
-        camera.render_overlay = render_overlay
-        signal.pause()
+
+def run_server(interpreter, labels, args):
+    # grab global references to the video stream, output frame, and
+    # lock variables
+    global outputFrame, lock
+    # initialize the motion detector and the total number of frames
+    cam = cv2.VideoCapture(args.camera_idx)
+    assert cam is not None
+    while True:
+        try:
+            res, image = cam.read()
+            if res is False:
+                logger.error("Empty image received")
+                break
+            else:
+                timestamp = time.time()
+                inference_size = input_size(interpreter)
+                cv2_im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+                run_inference(interpreter, cv2_im_rgb.tobytes())
+                objs = get_objects(interpreter, args.threshold)[:args.top_k]
+                frame = append_objs_to_img(image, inference_size, objs, labels)
+
+                tinference = time.time() - timestamp
+                logger.info("Frame done in {}".format(tinference))
+                cv2.putText(image, datetime.datetime.now().strftime(
+                    "%A %d %B %Y %I:%M:%S%p"), (10, image.shape[0] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+                with lock:
+                    outputFrame = frame.copy()
+
+        except KeyboardInterrupt:
+            break
+
+    cam.release()
+
 
 def main():
     default_model_dir = '../all_models'
@@ -77,14 +105,14 @@ def main():
     default_labels = 'coco_labels_de.txt'
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir,default_model))
+                        default=os.path.join(default_model_dir, default_model))
     parser.add_argument('--labels', help='label file path',
                         default=os.path.join(default_model_dir, default_labels))
     parser.add_argument('--top_k', type=int, default=3,
                         help='number of categories with highest score to display')
-    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default=0)
+    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default=1)
     parser.add_argument('--threshold', type=float, default=0.5,
-                        help='classifier score threshold')
+                        help='getector score threshold')
     args = parser.parse_args()
 
     print('Loading {} with {} labels.'.format(args.model, args.labels))
@@ -93,26 +121,33 @@ def main():
     labels = read_label_file(args.labels)
     inference_size = input_size(interpreter)
 
-    cap = cv2.VideoCapture(args.camera_idx)
+    t = threading.Thread(target=run_server, args=(interpreter, labels, inference_size, args.device))
+    t.daemon = True
+    t.start()
+    # start the flask app
+    app.run(host=args.ip, port=args.port, debug=True, threaded=True, use_reloader=False)
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        cv2_im = frame
 
-        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
-        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
-        run_inference(interpreter, cv2_im_rgb.tobytes())
-        objs = get_objects(interpreter, args.threshold)[:args.top_k]
-        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels)
+def generate():
+    # grab global references to the output frame and lock variables
+    global outputFrame, lock
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
+        # yield the output frame in the byte format
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+               bytearray(encodedImage) + b'\r\n')
 
-        # cv2.imshow('frame', cv2_im)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-
-    cap.release()
-    # cv2.destroyAllWindows()
 
 def append_objs_to_img(cv2_im, inference_size, objs, labels):
     height, width, channels = cv2_im.shape
@@ -126,9 +161,17 @@ def append_objs_to_img(cv2_im, inference_size, objs, labels):
         label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
 
         cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+        cv2_im = cv2.putText(cv2_im, label, (x0, y0 + 30),
                              cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
     return cv2_im
+
+
+@app.route("/video_feed")
+def video_feed():
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
 
 if __name__ == '__main__':
     main()
