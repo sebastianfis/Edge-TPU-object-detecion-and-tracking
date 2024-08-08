09d972fc6d23fa215bb4c644a9671a809f990b56
Sebastian <sebastian_fis@yahoo.de>
2024-08-08 18:19:10 +0200

    corrected object handling

--- opencv/detect_server.py
+++ opencv/detect_server.py
@@ -38,7 +38,7 @@ import numpy as np
 import collections
 
 from pycoral.adapters.common import input_size, output_tensor
-from pycoral.adapters.detect import Object, BBox
+from pycoral.adapters.detect import Object, BBox, get_objects
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
