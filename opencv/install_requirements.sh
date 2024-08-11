ece0eec636eaa2140c38c7a3fdb335dde768c469
Sebastian <sebastian_fis@yahoo.de>
2024-08-11 19:43:58 +0200

    corrected installation script

--- opencv/install_requirements.sh
+++ opencv/install_requirements.sh
@@ -21,7 +21,7 @@ if grep -s -q "Mendel" /etc/os-release; then
     echo "You must upgrade to Mendel 4.0 or higher."
     exit 1
   fi
-  sudo sudo python3 -m pip install opencv-python-headless==4.7.0.72
+  sudo pip install opencv-python-headless==4.7.0.72
 elif grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
   RASPBIAN=$(grep VERSION_ID /etc/os-release | sed 's/VERSION_ID="\([0-9]\+\)"/\1/')
   echo "Raspbian Version: $RASPBIAN"
@@ -46,7 +46,7 @@ echo "Note that the trackers have their own licensing, many of which
 are not Apache. Care should be taken if using a tracker with restrictive
 licenses for end applications."
 
-read -p "Install SORT (GPLv3)? (Y/N): " confirm && [[ $confirm = [yY] || $confirm = [yY][eE][sS] ]] || exit 1
+read -p "Install SORT (GPLv3)? (Y/N): " confirm && [ $confirm = [yY] ] || [ $confirm = [yY][eE][sS] ] || exit 1
 wget https://github.com/abewley/sort/archive/master.zip -O sort.zip
 unzip sort.zip -d ../third_party
 rm sort.zip
